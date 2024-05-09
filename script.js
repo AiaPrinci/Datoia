f6eff8c0b4a58bf11ad46e186e9641a4801e9ee7
AiaPrinci <davidjimsheleishvili17@gmail.com>
2024-05-09 15:58:08 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let bodyContainer = document.querySelector('.body-contianer');
+let box = document.querySelector('.box');
+let boxLayer = document.createElement('div');
+boxLayer.classList.add('box-layer');
+boxLayer.style.visibility = 'hidden';
+let instagram = document.querySelector('.instagram');
+
+box.appendChild(boxLayer);
+boxLayer.appendChild(instagram);
+
+box.addEventListener('mouseenter', () => {
+    boxLayer.style.visibility = 'visible';
+});
+
+box.addEventListener('mouseleave', () => {
+    boxLayer.style.visibility = 'hidden';
+});
+
+const click = () => {
+    box.addEventListener('click', () => {
+        boxLayer.style.visibility = 'visible';
+    });
+};
+
+click();
